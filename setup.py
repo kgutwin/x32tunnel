a362021d8912d020f89fc583683855d26e94ed28
Karl Gutwin <karl@gutwin.org>
2021-09-09 08:54:38 -0400

    Start downgrading for py3.2 support

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ requirements = []
 
 setup(
     name='x32tunnel',
-    use_scm_version=True,
+    version='0.1-alpha',
+    #use_scm_version=True,
     author="Karl Gutwin",
     author_email="karl@gutwin.org",
     description="X32 OSC TCP tunnel",
@@ -14,8 +15,8 @@ setup(
     entry_points={
         'console_scripts': ['x32tunnel=x32tunnel.cli:main']
     },
-    python_requires='>=3.6',
-    setup_requires=['setuptools_scm'],
+    python_requires='>=3.2',
+    #setup_requires=['setuptools_scm'],
     install_requires=requirements,
     classifiers=[
         'Programming Language :: Python :: 3'
