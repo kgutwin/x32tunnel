1d914ffd4e0eb7dec0e9c03f492963b86b742636
Karl Gutwin <karl@gutwin.org>
2021-09-12 16:00:31 -0400

    Don't forget to start the thread

--- x32tunnel/mixer_side.py
+++ x32tunnel/mixer_side.py
@@ -24,7 +24,7 @@ class TunnelConnections(utils.MultiConnections):
         self.conns[address] = sock
         self.addresses[sock] = address
         self.queues[sock] = queue.LifoQueue(maxsize=8)
-        threading.Thread(target=self.send_thread, args=[sock])
+        threading.Thread(target=self.send_thread, args=[sock]).start()
 
     def open_socket(self):
         raise Exception("should not get here")
